1cbb4ff8f33c19dd1fd5608a3c0bc326a4aecc5d
Erlan <bakytzanoverlan234@gmail.com>
2026-06-29 10:23:09 +0500

    Upgrade NetworkConfig

--- app/src/main/java/com/ereke/ai/ai/NetworkConfig.kt
+++ app/src/main/java/com/ereke/ai/ai/NetworkConfig.kt
@@ -1,17 +1,37 @@
 package com.ereke.ai.ai
 
+import okhttp3.ConnectionPool
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import java.time.Duration
+import java.util.concurrent.TimeUnit
 
 object NetworkConfig {
 
+    private val logger = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BASIC
+    }
+
     val client: OkHttpClient by lazy {
         OkHttpClient.Builder()
-            .connectTimeout(Duration.ofSeconds(30))
-            .readTimeout(Duration.ofSeconds(60))
-            .writeTimeout(Duration.ofSeconds(60))
-            .callTimeout(Duration.ofSeconds(90))
+
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .callTimeout(90, TimeUnit.SECONDS)
+
             .retryOnConnectionFailure(true)
+
+            .connectionPool(
+                ConnectionPool(
+                    5,
+                    5,
+                    TimeUnit.MINUTES
+                )
+            )
+
+            .addInterceptor(logger)
+
             .build()
     }
 }
